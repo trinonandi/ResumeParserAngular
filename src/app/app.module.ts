7c440c232dbafcac4d3bd2f52f59a8d5d5f0c40a
Trinanjan Nandi <trino.nandi@gmail.com>
2024-06-06 13:50:23 -0700

    Add HTTP service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DragNDropDirective } from './directives/drag-n-drop.directive';
+import { UploadPdfComponent } from './upload-pdf/upload-pdf.component';
+import {provideHttpClient} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DragNDropDirective,
+    UploadPdfComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/upload-pdf/upload-pdf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-pdf',
+  templateUrl: './upload-pdf.component.html',
+  styleUrl: './upload-pdf.component.css'
+})
+export class UploadPdfComponent {
+
+}
